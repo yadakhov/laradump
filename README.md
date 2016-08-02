49d1c09544523b6622358c9e890617b934ef99db
Yada Khov <ykhov@williamhill.us>
2016-08-02 09:27:11 -0700

    Add package base files.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Laradump is a mysqldump wrapper for laravel.
---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+class BootstrapTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+}
